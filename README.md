6c0de6c66741b8bace849f583968744ac49276bf
Laura Orchard <artist@lauraorchard.com>
2015-04-16 19:46:33 -0600

    Created the documentation directory and added a draft version of the milestone-1.php

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # lorchard
-The Small Orange domain 
+The Small Orange domain
+A website to showcase Laura's fine art portfolio, her drawing classes, and provide a place for buyers to purchase art prints online.
---
+++ documentation/milestone_1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="UTF-8">
+		<title>Laura O's Personal Website - Milestone 1</title>
+	</head>
+	<header><h3>Laura O's Personal Website - Milestone 1</h3></header>
+	<body>
+
+		<h3>Purpose:</h3>
+		<p>The purpose of my website to serve as a virtual exhibition space for my current work, a virtual museum space for my
+		older work, and to provide a way for students to learn about my classes and collectors to purchase my work.</p>
+		<h3>Audience:</h3>
+		<p>The audience is comprised of art collectors looking to purchase fine ar, art students looking for training,
+		and other artists keeping tabs on new bodies of work.</p>
+		<h3>Goals:</h3>
+			<ul>
+				<li>To present my fine art in a sophisticated format.</li>
+				<li>To increase sales of originals and prints.</li>
+				<li>To make information about my drawing classes more accessible.</li>
+			</ul>
+	</body>
+</html>
