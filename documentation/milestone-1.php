30afc0988636f855a4e7a18fac5294b7b1091fb0
Laura Orchard <artist@lauraorchard.com>
2015-04-19 13:24:47 -0600

    Edited the READEME.md file and the milestone-1.php document to reflect web development, rather than an art focus.
    Minor edit to the milestone-1 file.
    
    Testing the push to SmallOrange.
    
    Testing the push to SmallOrange.
    
    Edited the text of milestone-1 file.
    
    Completely rewrote this file to more accurately reflect my thoughts.

--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -7,15 +7,15 @@
 	<header><h3>Laura Orchard's Personal Website - Milestone 1</h3></header>
 	<body>
 		<h3>Purpose:</h3>
-		<p>The purpose of my website is to promote my coding, web development, project management, and other IT skills
-			in order to gain contract or corporate employment.</p>
+		<p>The purpose of lorchard.org is to promote my skills as a full stack developer and software development project manager while with a focus on designing a better world.
+		</p>
 		<h3>Audience:</h3>
-		<p>The audience will be any person or organization that has a need for my skills, and whose values are in alignment with my own.
-		I prefer to work with organizations - or on products - that will enhance or benefit life on this planet in one way or another.</p>
+		<p>The audience for my site is comprised of clients with software and web site development projects, as well as other developers building teams of expertise.</p>
 		<h3>Goals:</h3>
 			<ul>
-				<li>To attract clients and colleagues in the web development field to hire me;</li>
-				<li>To connect with like-minded people who strive to work for the greater good.</li>
+				<li>To create a consistent flow of income through contract projects with fellow developers;</li>
+				<li>To convey a reputation for integrity and excellence;</li>
+				<li>To interact with a community of like-minded altruistic coders.</li>
 			</ul>
 	</body>
 </html>
