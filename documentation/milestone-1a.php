af2c4003dfb488d5913aa9c857d85bf931c88646
Laura Orchard <artist@lauraorchard.com>
2015-04-18 22:46:35 -0600

    Edited the READEME.md file and the milestone-1.php document to reflect web development, rather than an art focus.
    Minor edit to the milestone-1 file.
    
    Testing the push to SmallOrange.

---
+++ documentation/milestone-1a.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="UTF-8">
+		<title>Laura O's PWP - Milestone 1</title>
+	</head>
+	<header><h3>Laura Orchard's Personal Website - Milestone 1</h3></header>
+	<body>
+		<h3>Purpose:</h3>
+		<p>The purpose of my website is to promote my coding, web development, project management, and other IT skills
+			in order to gain contract or corporate employment.</p>
+		<h3>Audience:</h3>
+		<p>The audience will be any person or organization that has a need for my skills, and whose values are in alignment with my own.
+		I prefer to work with organizations or on products that will enhance or benefit life on this planet in one way or another.</p>
+		<h3>Goals:</h3>
+			<ul>
+				<li>To present my information in a well-written, creative, eye-catching (but technically sophisticated) format;</li>
+				<li>To make it easy for clients to understand what my expertise is;</li>
+				<li>To make my portfolio of work quickly accessible;</li>
+				<li>To make it easy to contact me.</li>
+			</ul>
+	</body>
+</html>
