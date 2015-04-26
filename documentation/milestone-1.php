ee6adc6b5219495252f797f5da6a28254a374a12
Laura Orchard <artist@lauraorchard.com>
2015-04-25 18:01:40 -0600

    Moved <header> to within <body> and un-indented the <ul>/<li> lines to reflect the proper parent-child relationship.

--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -4,18 +4,18 @@
 		<meta charset="UTF-8">
 		<title>Laura O's PWP - Milestone 1</title>
 	</head>
-	<header><h3>Laura Orchard's Personal Website - Milestone 1</h3></header>
 	<body>
+		<header><h3>Laura Orchard's Personal Website - Milestone 1</h3></header>
 		<h3>Purpose:</h3>
 		<p>The purpose of lorchard.org is to promote my skills as a full stack developer and software development project manager while with a focus on designing a better world.
 		</p>
 		<h3>Audience:</h3>
 		<p>The audience for my site is comprised of clients with software and web site development projects, as well as other developers building teams of expertise.</p>
 		<h3>Goals:</h3>
-			<ul>
-				<li>To create a consistent flow of income through contract projects with fellow developers;</li>
-				<li>To convey a reputation for integrity and excellence;</li>
-				<li>To interact with a community of like-minded altruistic coders.</li>
-			</ul>
+		<ul>
+			<li>To create a consistent flow of income through contract projects with fellow developers;</li>
+			<li>To convey a reputation for integrity and excellence;</li>
+			<li>To interact with a community of like-minded altruistic coders.</li>
+		</ul>
 	</body>
 </html>
