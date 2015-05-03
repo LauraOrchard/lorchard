976b82cd09fd434577cda4612fdb55fd23ea46d2
Laura Orchard <artist@lauraorchard.com>
2015-05-02 18:29:00 -0600

    Beginning to format Milestone 2 in html.

---
+++ documentation/milestone-2.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="UTF-8">
+		<title>Milestone-2 for LOrchard.org</title>
+	</head>
+	<body>
+    <p>
+		Created by: 	Laura Orchard
+      Created for: 	Deep Dive Coding Bootcamp, PWP Project Milestone 2
+      Date:		April 28, 2015
+      URL:		http://lorchard.org
+
+  	<h1>Laura Orchard Personal Website Project - Milestone II</h1>
+
+  	<h3>Introduction:</h3>
+
+  	<p>Below is an outline for the User Experience and Interaction Design for the Laura Orchard Personal Website Project.
+	  This project is a professional portfolio website with a focus on web development and software implementation project
+	  management. The target audiences include charitable organizations with web development or implementation project
+	  management needs, and developers of products and applications that are globally beneficial or support sustainable
+	  living. The primary goal of this site is to provide a way for potential clients, hiring managers, and contract
+	  employers to see my work and contact me for employment opportunities.</p>
+
+	<h3>Personas<h3>
+
+	<p><strong>Persona #1: Private Client</strong></p>
+	<ul>
+		<li><strong>Name: Santa Fe Meditation Circle</strong></li>
+		<li><strong>Composition: </strong>an informal group (a circle) with a membership of 50-plus and a 3-person board of
+		directors. The circle is comprised of members from a highly respected worldwide organization, but the circle is not
+		officially affiliated with parent group for legal and tax reasons. Median age of the members is 60.</li>
+		<li><strong>Technology: </strong>No dedicated technology – each year the administrative tasks and related documents are
+		passed from one board to next. The membership uses email, mobile phones and desktops, but are generally not tech-savvy.
+		Volunteers use their own equipment to manage the website, mailing list and documents.</li>
+		<li><strong>Attitudes and Behaviors: </strong>With an older membership, the value of technology and development is
+		misunderstood and underestimated – both in terms of cost and functionality. They want something that looks like what
+		the larger (more affluent) organizations have. Without an understanding of what is involved, they may not want to pay
+		full price (or at all) for the design, development and maintenance. For the past 10 years, one member has taken it
+		upon himself to donate his time, using a free template-based site builder and hosting agreement, covering the annual
+		cost on his own. He is ready to ditch the old site in favor of one that is more up to date and easy to maintain,
+		ultimately passing the responsibility back to the circle as an anticipated annual expense for them to include in
+		their budget. All expenses for the circle are covered by donations.</li>
+		<li><strong>Frustrations and Needs: </strong></li>
+			<ul>
+				<li>Budget – there has never been a line item for technology (only rent and utilities for the dedicated meeting space).</li>
+				<li>A demonstration of value will be necessary to prove that a professional website is the right expenditure for this group
+				– they will need to see an invoice for the retail price and maintenance agreement.</li>
+				<li>The circle needs a site that will emulate the parent organization’s website, providing the circle’s basic protocol,
+				schedule, contact information, and e-news sign up form.</li>
+				<li>Updates to the content normally occur monthly.</li>
+				<li>Currently, the mailing list and monthly newsletters are handled using MailChimp, but the template is a default
+				template without any branding.</li>
+				<li>Google Calendar is used for volunteer sign up.</li>
+				<li>Documents are now electronic, but not stored on a shared site.</li>
+				<li>Training is a huge need from year to year as the board rotates off.</li>
+			</ul>
+		<li><strong>Goals:</strong> The circle needs an easy to maintain, low cost, professional-looking solution that will
+		incorporate a dynamic monthly calendar, a newsletter sign-up form, a branded newsletter template, easily editable
+		content, and a shared document management system. Clarity on what the annual costs and training that are needed to
+		maintain the site and the list. They also need a strategy regarding document management for a virtual office.</li>
+		</body>
+	</html>
