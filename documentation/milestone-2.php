a266ba102eb727930bc49cc4acb29cf148d6d6db
Laura Orchard <artist@lauraorchard.com>
2015-05-02 19:12:25 -0600

    Completed formatting the Personas in html.

--- documentation/milestone-2.php
+++ documentation/milestone-2.php
@@ -1,3 +1,7 @@
+<!- - Milestone II for the PWP Project includes: Key personas, a use case for each persona, a flowchart for each use case,
+a proposed sitemap, and proposed features and functionality. - - >
+
+
 <!DOCTYPE html>
 <html>
 	<head lang="en">
@@ -5,15 +9,16 @@
 		<title>Milestone-2 for LOrchard.org</title>
 	</head>
 	<body>
-    <p>
-		Created by: 	Laura Orchard
-      Created for: 	Deep Dive Coding Bootcamp, PWP Project Milestone 2
-      Date:		April 28, 2015
-      URL:		http://lorchard.org
+
 
   	<h1>Laura Orchard Personal Website Project - Milestone II</h1>
+		<p>
+			Created by: 	Laura Orchard<br />
+			Created for: 	Deep Dive Coding Bootcamp, PWP Project Milestone II<br />
+			Date:		May 2, 2015<br />
+			URL:		http://lorchard.org</p>
 
-  	<h3>Introduction:</h3>
+  	<h2>Introduction:</h2>
 
   	<p>Below is an outline for the User Experience and Interaction Design for the Laura Orchard Personal Website Project.
 	  This project is a professional portfolio website with a focus on web development and software implementation project
@@ -22,42 +27,95 @@
 	  living. The primary goal of this site is to provide a way for potential clients, hiring managers, and contract
 	  employers to see my work and contact me for employment opportunities.</p>
 
-	<h3>Personas<h3>
+	<h2>Personas</h2>
+		<p><strong>Persona #1: Contract Client</strong></p>
+		<ul>
+			<li><strong>Name: </strong>Julie Criss Gibson, founder of SJConsulting</li>
+			<li><strong>Age: </strong>42</li>
+			<li><strong>Technology: </strong>SJ Consulting works in a multitude of content management systems, including
+				NetCommunity (Pro, Grow and Spark), Sphere/FAF, Luminate/TeamRaiser, Drupal, WordPress, Sitefinity, Ektron,
+				and/or custom platforms. SJC has a team of designers, programmers, and consultants with expertise of online
+				and offline databases, email marketing, and software platforms to meet the full spectrum of clients' needs.</li>
+			<li><strong>Attitudes and Behaviors: </strong>Dedicated to working for nonprofits with complex needs, Julie is
+				also very keen to create a strong female coding team. Her approach with clients always includes a strategic
+				view of the clients’ project that extends to training in email marketing as well as online tools that make
+				fundraising and event management seamless to the public. Her approach with her coding team includes mentoring
+				and guidance in order to allow each member to improve and excel in their area of expertise while maintaining
+				a balanced life outside her company.</li>
+			<li><strong>Frustrations and Needs: </strong>The high volume of requests from the nonprofit sector seeking
+				professional solutions to complex technology problems means the need for competent programmers is constant.
+				Determining the right technology solutions for each client is taking over Julie’s schedule and she needs to
+				feel comfortable completely relying on her staff to execute high quality products and take over the group
+				training sessions.</li>
+			<li><strong>Goals: </strong>Julie is in need of skilled coding talent from across the country with cross-over
+				skills in both project management and technical training for hourly or project-based employment.</li>
+		</ul>
 
-	<p><strong>Persona #1: Private Client</strong></p>
-	<ul>
-		<li><strong>Name: Santa Fe Meditation Circle</strong></li>
-		<li><strong>Composition: </strong>an informal group (a circle) with a membership of 50-plus and a 3-person board of
-		directors. The circle is comprised of members from a highly respected worldwide organization, but the circle is not
-		officially affiliated with parent group for legal and tax reasons. Median age of the members is 60.</li>
-		<li><strong>Technology: </strong>No dedicated technology – each year the administrative tasks and related documents are
-		passed from one board to next. The membership uses email, mobile phones and desktops, but are generally not tech-savvy.
-		Volunteers use their own equipment to manage the website, mailing list and documents.</li>
-		<li><strong>Attitudes and Behaviors: </strong>With an older membership, the value of technology and development is
-		misunderstood and underestimated – both in terms of cost and functionality. They want something that looks like what
-		the larger (more affluent) organizations have. Without an understanding of what is involved, they may not want to pay
-		full price (or at all) for the design, development and maintenance. For the past 10 years, one member has taken it
-		upon himself to donate his time, using a free template-based site builder and hosting agreement, covering the annual
-		cost on his own. He is ready to ditch the old site in favor of one that is more up to date and easy to maintain,
-		ultimately passing the responsibility back to the circle as an anticipated annual expense for them to include in
-		their budget. All expenses for the circle are covered by donations.</li>
-		<li><strong>Frustrations and Needs: </strong></li>
-			<ul>
-				<li>Budget – there has never been a line item for technology (only rent and utilities for the dedicated meeting space).</li>
-				<li>A demonstration of value will be necessary to prove that a professional website is the right expenditure for this group
-				– they will need to see an invoice for the retail price and maintenance agreement.</li>
-				<li>The circle needs a site that will emulate the parent organization’s website, providing the circle’s basic protocol,
-				schedule, contact information, and e-news sign up form.</li>
-				<li>Updates to the content normally occur monthly.</li>
-				<li>Currently, the mailing list and monthly newsletters are handled using MailChimp, but the template is a default
-				template without any branding.</li>
-				<li>Google Calendar is used for volunteer sign up.</li>
-				<li>Documents are now electronic, but not stored on a shared site.</li>
-				<li>Training is a huge need from year to year as the board rotates off.</li>
-			</ul>
-		<li><strong>Goals:</strong> The circle needs an easy to maintain, low cost, professional-looking solution that will
-		incorporate a dynamic monthly calendar, a newsletter sign-up form, a branded newsletter template, easily editable
-		content, and a shared document management system. Clarity on what the annual costs and training that are needed to
-		maintain the site and the list. They also need a strategy regarding document management for a virtual office.</li>
-		</body>
-	</html>
+		<p><strong>Persona #2: Employer</strong></p>
+		<ul>
+			<li><strong>Name: </strong>Fred Armisted, Chief Strategy Officer, Fusion Labs</li>
+			<li><strong>Age: </strong>65</li>
+			<li><strong>Technology: </strong>Creator of enterprise level scholarship and grants management software for
+				universities and nonprofits with sophisticated online interfaces; his company provides technical consulting;
+				system management and hosting; proprietary nonprofit software; and advanced technology implementation.</li>
+			<li><strong>Attitudes and Behaviors: </strong>Fusion Labs has been growing steadily for 5 years and is always
+				looking for more coding talent and project managers to keep their project pipeline flowing. Understaffed,
+				Fusion too often sends the wrong skills set to the clients for training and/or installations, which has made
+				the roll-outs less than successful for understaffed clients. The time it takes to get a charitable
+				organization up and running has been getting longer as the programming staff has taken on more peripheral tasks.</li>
+			<li><strong>Frustrations and Needs: </strong>The success of Fusion’s products have taken a bite out of their
+				chief competitor, Blackbaud, and they need to maintain this edge in order to remain a viable company. With
+				more organizations signing on for installations, they are short on programmers with experience in their
+				platform. Disorganized and/or poor project implementation has caused some clients to grumble that could
+				cost them future business.</li>
+			<li><strong>Goals: </strong>To hire 9 senior programmers and engineers and 2 project managers as soon as
+				possible, and then determine the junior positions needed to fill in the gaps. The longer-term goal is to
+				increase loyalty and retention so the teams will be more cohesive and productive with less distraction and
+				more seamless installations.</li>
+		</ul>
+
+		<p><strong>Persona #3: Private Client (potential pro-bono project)</strong></p>
+		<ul>
+			<li><strong>Name: </strong>Santa Fe Meditation Circle</li>
+			<li><strong>Composition: </strong>an informal group (a circle) with a membership of 50-plus and a 3-person board of
+			directors. The circle is comprised of members from a highly respected worldwide organization, but the circle is not
+			officially affiliated with parent group for legal and tax reasons. Median age of the members is 60.</li>
+			<li><strong>Technology: </strong>No dedicated technology – each year the administrative tasks and related documents are
+			passed from one board to next. The membership uses email, mobile phones and desktops, but are generally not tech-savvy.
+			Volunteers use their own equipment to manage the website, mailing list and documents.</li>
+			<li><strong>Attitudes and Behaviors: </strong>With an older membership, the value of technology and development is
+			misunderstood and underestimated – both in terms of cost and functionality. They want something that looks like what
+			the larger (more affluent) organizations have. Without an understanding of what is involved, they may not want to pay
+			full price (or at all) for the design, development and maintenance. For the past 10 years, one member has taken it
+			upon himself to donate his time, using a free template-based site builder and hosting agreement, covering the annual
+			cost on his own. He is ready to ditch the old site in favor of one that is more up to date and easy to maintain,
+			ultimately passing the responsibility back to the circle as an anticipated annual expense for them to include in
+			their budget. All expenses for the circle are covered by donations.</li>
+			<li><strong>Frustrations and Needs: </strong></li>
+				<ul>
+					<li>Budget – there has never been a line item for technology (only rent and utilities for the dedicated meeting space).</li>
+					<li>A demonstration of value will be necessary to prove that a professional website is the right expenditure for this group
+					– they will need to see an invoice for the retail price and maintenance agreement.</li>
+					<li>The circle needs a site that will emulate the parent organization’s website, providing the circle’s basic protocol,
+					schedule, contact information, and e-news sign up form.</li>
+					<li>Updates to the content normally occur monthly.</li>
+					<li>Currently, the mailing list and monthly newsletters are handled using MailChimp, but the template is a default
+					template without any branding.</li>
+					<li>Google Calendar is used for volunteer sign up.</li>
+					<li>Documents are now electronic, but not stored on a shared site.</li>
+					<li>Training is a huge need from year to year as the board rotates off.</li>
+				</ul>
+			<li><strong>Goals:</strong> The circle needs an easy to maintain, low cost, professional-looking solution that will
+			incorporate a dynamic monthly calendar, a newsletter sign-up form, a branded newsletter template, easily editable
+			content, and a shared document management system. Clarity on what the annual costs and training that are needed to
+			maintain the site and the list. They also need a strategy regarding document management for a virtual office.</li>
+		</ul>
+
+
+
+
+
+
+
+	</body>
+</html>
