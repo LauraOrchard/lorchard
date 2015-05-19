a4d5d785164ebc1d9e404ea51e7c96bbac26ed52
Laura Orchard <artist@lauraorchard.com>
2015-05-19 12:49:24 -0600

    Moving nav tabs

--- images/homepage.php
+++ images/homepage.php
@@ -6,10 +6,10 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 		<!-- Bootstrap Latest compiled and minified CSS -->
-		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet"/>
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet"/>
 
 		<!-- Optional Bootstrap theme -->
-		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css" rel="stylesheet"/>
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css" rel="stylesheet"/>
 
 		<!-- ////////////////////////////////////////////////
 		 //// LINK TO YOUR CUSTOM CSS FILES HERE
@@ -24,40 +24,18 @@
 		<![endif]-->
 
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/additional-methods.min.js"></script>
 
 		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
-		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 		<title>Bootstrap 101 Template (DDC Version!)</title>
 	</head>
 	<!--The actual page-->
 	<body>
 		<div class="container">
-			<div class="row">
-				<!--The following justified buttons will either be anchors on this page or to secondary pages-->
-				<div class="col-lg-12">
-					<div class="btn-group btn-group-justified" role="group" aria-label="...">
-						<div class="btn-group" role="group">
-							<button type="button" class="btn btn-default">Home</button>
-						</div>
-						<div class="btn-group" role="group">
-							<button type="button" class="btn btn-default">About</button>
-						</div>
-						<div class="btn-group" role="group">
-							<button type="button" class="btn btn-default">Services</button>
-						</div>
-						<div class="btn-group" role="group">
-							<button type="button" class="btn btn-default">Work</button>
-						</div>
-						<div class="btn-group" role="group">
-							<button type="button" class="btn btn-default">Contact</button>
-						</div>
-					</div>
-				</div>
-			</div>
 
 		<div class="row">
 			<!--This is a responsive png logo made in Photoshop-->
@@ -109,6 +87,28 @@
 				</a>
 			</div>
 			<!-- Carousel -->
+			<div class="row">
+				<!--The following justified buttons will either be anchors on this page or to secondary pages-->
+				<div class="col-lg-12">
+					<div class="btn-group btn-group-justified" role="group" aria-label="...">
+						<div class="btn-group" role="group">
+							<button type="button" class="btn btn-default">Home</button>
+						</div>
+						<div class="btn-group" role="group">
+							<button type="button" class="btn btn-default">About</button>
+						</div>
+						<div class="btn-group" role="group">
+							<button type="button" class="btn btn-default">Services</button>
+						</div>
+						<div class="btn-group" role="group">
+							<button type="button" class="btn btn-default">Work</button>
+						</div>
+						<div class="btn-group" role="group">
+							<button type="button" class="btn btn-default">Contact</button>
+						</div>
+					</div>
+				</div>
+			</div>
 			<h1>Hello, world! I'm a Bootstrap page! :D</h1>
 		</div>
 	</body>
