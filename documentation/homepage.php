7c3d5c307d6693b507d5d55a9c663fce9d084289
Laura Orchard <artist@lauraorchard.com>
2015-05-24 20:34:52 -0600

    Changing navbar css.

--- documentation/homepage.php
+++ documentation/homepage.php
@@ -41,8 +41,9 @@
 		<body>
 			<div class="navbar-wrapper">
 				<div class="container">
-
-					<nav class="navbar navbar-inverse navbar-static-top">
+					<div class="navbar transparent navbar-inverse navbar-fixed-top">
+						<nav class="navbar-inner">
+					<!--original inverse <nav class="navbar navbar-inverse navbar-static-top"> -->
 						<div class="container">
 							<div class="navbar-header">
 								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -50,15 +51,18 @@
 									<span class="icon-bar"></span>
 									<span class="icon-bar"></span>
 									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
 								</button>
-								<a class="navbar-brand" href="#">Project name</a>
+								<a class="navbar-brand" href="#">Laura O</a>
 							</div>
 							<div id="navbar" class="navbar-collapse collapse">
 								<ul class="nav navbar-nav">
 									<li class="active"><a href="#">Home</a></li>
 									<li><a href="#about">About</a></li>
+									<li><a href="#about">Services</a></li>
+									<li><a href="#about">Portfolio</a></li>
 									<li><a href="#contact">Contact</a></li>
-									<li class="dropdown">
+									<!--<li class="dropdown">
 										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
 										<ul class="dropdown-menu" role="menu">
 											<li><a href="#">Action</a></li>
@@ -68,7 +72,7 @@
 											<li class="dropdown-header">Nav header</li>
 											<li><a href="#">Separated link</a></li>
 											<li><a href="#">One more separated link</a></li>
-										</ul>
+										</ul>-->
 									</li>
 								</ul>
 							</div>
@@ -90,17 +94,17 @@
 				</ol>
 				<div class="carousel-inner" role="listbox">
 					<div class="item active">
-						<img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+						<img class="first-slide" src="../images/STEMgrate.jpg" alt="First slide">
 						<div class="container">
 							<div class="carousel-caption">
 								<h1>Introducing...</h1>
-								<p>Laura Orchard, a design-centric web developer and IT project manager.</p>
+								<p>Laura Orchard</p>
 								<!-- <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p> -->
 							</div>
 						</div>
 					</div>
 					<div class="item">
-						<img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+						<img class="second-slide" src="../images/NMStatePrintingConcrete.jpg" alt="Second slide">
 						<div class="container">
 							<div class="carousel-caption">
 								<h1>Doing good in the world requires good programming.</h1>
@@ -110,10 +114,10 @@
 						</div>
 					</div>
 					<div class="item">
-						<img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+						<img class="third-slide" src="../images/PlanterConcrete.jpg" alt="Third slide">
 						<div class="container">
 							<div class="carousel-caption">
-								<h1>Teamwork requires the space to be your best.</h1>
+								<h1>Teamwork is an opportunity.</h1>
 								<p>The best projects allow the coders to focus on code, the architects to focus on planning, and
 									the designers to create something the client is excited about.</p>
 								<!--<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>-->
@@ -141,20 +145,20 @@
 				<!-- Three columns of text below the carousel -->
 				<div class="row">
 					<div class="col-lg-4">
-						<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-						<h2>Heading</h2>
+						<img class="img-square" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+						<h2>Project Management</h2>
 						<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
 						<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
 					</div><!-- /.col-lg-4 -->
 					<div class="col-lg-4">
 						<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-						<h2>Heading</h2>
+						<h2>Design</h2>
 						<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
 						<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
 					</div><!-- /.col-lg-4 -->
 					<div class="col-lg-4">
-						<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-						<h2>Heading</h2>
+						<img class="img-square" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+						<h2>Teamwork</h2>
 						<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
 						<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
 					</div><!-- /.col-lg-4 -->
