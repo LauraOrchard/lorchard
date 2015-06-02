30537230d7299db9f708857b4236552cf5e431d1
Laura Orchard <artist@lauraorchard.com>
2015-06-01 21:27:59 -0600

    Created the About page.

---
+++ documentation/about.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet"/>
+
+		<!-- Optional Bootstrap theme -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css" rel="stylesheet"/>
+
+		<!-- ////////////////////////////////////////////////
+		 //// LINK TO YOUR CUSTOM CSS FILES HERE		 ///////////////////////////////////////////////////// -->
+		<link type="text/css" href="../css/covercustom.css" rel="stylesheet"/>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/additional-methods.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+		<title>LOrchard.org</title>
+	</head>
+	<!--The actual page-->
+
+	<body>
+
+		<div class="site-wrapper">
+
+			<div class="site-wrapper-inner">
+
+				<div class="cover-container">
+
+					<div class="masthead clearfix">
+						<div class="inner">
+							<h3 class="masthead-brand">Laura Orchard</h3>
+							<nav>
+								<ul class="nav masthead-nav">
+									<li class="active"><a href="http://lorchard.org/documentation/newhome.php">Home</a></li>
+									<li><a href="http://lorchard.org/documentation/Services.php">Services</a></li>
+									<li><a href="#">About</a></li>
+									<li><a href="#">Contact</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+<!--					<div class="container">-->
+<!--						<div class="row">-->
+<!--							<div class="col-lg-4">-->
+<!--								<h2>A little about me</h2>-->
+<!--								<p>I've taken to heart my mother's directive to "do something constructive". Whether-->
+<!--								creating fine art, tailoring, pouring concrete, or bettering myself, my intent has always been to-->
+<!--								be constructive, productive and leave a situation better than when I found it.<br />>-->
+<!--								An Ohioan by birth, and a New Yorker by choice, I discovered Santa Fe, New Mexico in 1993 on a-->
+<!--									painting trip and never left. The open skies of New Mexico are an invitation to remake yourself,-->
+<!--									and I find myself accepting that challenge over and over again.<br />-->
+<!--									The artistic and the technical sides of my creativity have led me to a variety of interesting-->
+<!--									careers that include everything from mural painting to project management and from ceramics to-->
+<!--									business process mapping.<br />-->
+<!---->
+<!--								</p>-->
+<!--							</div>-->
+<!--						</div>-->
+<!--					</div>-->
+
+					<div class="inner cover">
+<!--						<h1 class="cover-heading">IT'S A BRAND NEW DAY</h1>-->
+<!--						<p class="lead">Show off who you are with a brand new site!</p-->
+						<div class="container-about">
+							<div class="col-lg-4"
+							</div>
+							<div class="col-lg-4"
+
+							<h2>A little about me:</h2>
+							<p>I've taken to heart my mother's directive to "do something constructive". Whether
+						creating fine art, tailoring, pouring concrete, or bettering myself, my intent has always been to
+						be constructive, productive and leave a situation better than when I found it.<br />
+
+						An Ohioan by birth, and a New Yorker by choice, I discovered Santa Fe, New Mexico in 1993 on a
+						landscape painting trip and never left. The open skies of New Mexico are an invitation to remake yourself,
+						and I find myself accepting that challenge over and over again.<br />
+
+						Both the artistic and the technical sides of my creativity have led me to a variety of interesting
+						careers that include everything from mural painting to IT project management and from ceramics to
+						business process mapping. There's just something about Excel spreadsheets...<br />
+							</p>
+							</div>
+							<div class="col-lg-4"
+							</div>
+						</div
+<!--						<p><a href="http://lorchard.org/documentation/Services.php" button type="button" class="btn btn-primary btn-lg outline">Learn how I can help you...</button></a></p>-->
+<!---->
+<!--					</div>-->
+
+					<div class="mastfoot">
+						<div class="inner">
+							<p>Website by Laura O</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
