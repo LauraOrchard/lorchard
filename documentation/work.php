69bfc366bc7317135c6ac85ea0ade78014032ea5
Laura Orchard <artist@lauraorchard.com>
2015-06-07 11:26:12 -0600

    Added a Work page with thumbnails

---
+++ documentation/work.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!--this brings in all head links-->
+		<?php require_once("../lib/head-utils.html"); ?>
+		<title>Lorchard</title>
+	</head>
+	<body class="bg-sky site">
+
+		<!--header include-->
+		<?php require_once("../lib/header.html"); ?>
+
+		<!--all content lives here-->
+		<main class="site-content">
+
+			<div class="container" id="page-container">
+
+				<!--welcome message section-->
+				<section class="welcome">
+					<div class="row vertical-center-row">
+						<div class="col-xs-12">
+							<div class="col-md-8 col-md-offset-2">
+								<div class="cover-heading">
+									<h1>Work I've contributed to</h1>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+
+				<!--begin 1/3 column thumbnails-->
+				<section>
+					<div class="row">
+						<col class="col-md-4">
+							<div class="row">
+								<div class="col-sm-6 col-md-4">
+									<div class="thumbnail">
+										<img src="../images/SFCFThumbnail.jpg" alt="Santa Fe Community Foundation Website">
+										<div class="caption">
+											<h3>Santa Fe Community Foundation Website</h3>
+											<p>
+											<ul style="list-style-type:none">
+												<li>Role: Project Manager, content creation, and maintenance.</li>
+												<li>Scope: New website to allow the community to sign up for workshops and to allow the nonprofit
+													 community to apply for grant opportunities online.</li>
+												<li>Tools: NetCommunity and Proffer applications.</li>
+												<li>Partners: Cisneros Design; Blackbaud; Fusion Labs; SJ Consulting.</li>
+											</ul>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-6 col-md-4">
+									<div class="thumbnail">
+										<img src="../images/LMOsite.jpg" alt="Orchard Studio Website">
+										<div class="caption">
+											<h3>Orchard Studio Website</h3>
+											<p>
+											<ul style="list-style-type:none">
+												<li>Role: Design, development, photography, content, and maintenance.</li>
+												<li>Scope: New website to exhibit fine art.</li>
+												<li>Tools: iWeb</li>
+											</ul>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-6 col-md-4">
+									<div class="thumbnail">
+										<img src="..." alt="2013 Giving Together Catalogue"><a href="../images/2013gtc.pdf" alt="2013 Giving Together Catalogue"</a>
+										<div class="caption">
+											<h3>Giving Together Catalogue</h3>
+											<p>
+											<ul style="list-style-type:none">
+												<li>Role: Project manager for original concept, design, development, and iterative
+													project management for print production portion each quarter.</li>
+												<li>Scope: Produce a branded catalogue of nonprofit grant applications with each grant cycle,
+													 processing input from applications, and delivering the online and print versions to the
+													community.</li>
+												<li>Tools: InDesign, NetCommunity, and Proffer</li>
+												<li>Partner: Cisneros Design</li>
+											</ul>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div> <!--.row-->
+
+				</section>
+
+			</div><!--.container-->
+
+		</main>
+
+		<!--footer include-->
+		<?php require_once("../lib/footer.html"); ?>
+
+	</body>
+</html>
